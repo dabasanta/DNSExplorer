6697a97cce7a3569d2eadd0d982a787a88c12897
Danilo <dabasantaco@gmail.com>
2021-09-13 21:50:09 -0500

    Improved syntax & Performance.

--- DNSExplorer-minimal.sh
+++ DNSExplorer-minimal.sh
@@ -7,18 +7,18 @@ clean(){
   rm -rf /tmp/dnsecrecon;exit 0
 }
 doZoneTransfer(){
-    success=1;for nsi in $(cat /tmp/dnsexplorer/NameServers.txt);do host -l $1 $nsi | grep -i "has address" > /dev/null;if [[ $? -eq 0 ]];then echo -e "[+] NameServer $nsi accept ZoneTransfer\n";host -l $1 $nsi | grep -i "has address";success=0;else echo -e "[-] NameServer $nsi does not accept zone transfer";fi;done;return $success
+  success=1;for nsi in $(cat /tmp/dnsexplorer/NameServers.txt);do host -l "$1" "$nsi" | grep -i "has address" > /dev/null;if [[ $? -eq 0 ]];then echo -e "[+] NameServer $nsi accept ZoneTransfer\n";host -l "$1" "$nsi" | grep -i "has address";success=0;else echo -e "[-] NameServer $nsi does not accept zone transfer";fi;done;return $success
 }
 dictionaryAttack(){
-    curl -s https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/DNS/bitquark-subdomains-top100000.txt -o /tmp/dnsexplorer/bit.txt;if [ -f /tmp/dnsexplorer/bit.txt ];then cat /tmp/dnsexplorer/bit.txt|head -1000>/tmp/dnsexplorer/bitquark.txt;l=$(wc -l /tmp/dnsexplorer/bitquark.txt|awk '{print $1}');c=1;s=0;for fqdn in $(cat /tmp/dnsexplorer/bitquark.txt);do host $fqdn.$1 | head -1 | grep "has address";if [ $? -eq 0 ];then s=$(($s+1));fi;echo -ne "[++] Using entry:$c of $l.\r";c=$(($c+1));done;if [ $s -ge 1 ];then echo -e "\n[+] Found $s subdomains.";else echo -e "\n[!!] Found $s subdomains.";fi;else echo -e "[!!] Could not download dictionary from seclists url.";clean;fi
+    curl -s https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/DNS/bitquark-subdomains-top100000.txt -o /tmp/dnsexplorer/bit.txt;if [ -f /tmp/dnsexplorer/bit.txt ];then cat /tmp/dnsexplorer/bit.txt|head -1000>/tmp/dnsexplorer/bitquark.txt;l=$(wc -l /tmp/dnsexplorer/bitquark.txt|awk '{print $1}');c=1;s=0;for fqdn in $(cat /tmp/dnsexplorer/bitquark.txt);do host $fqdn.$1 | head -1 | grep "has address";if [ $? -eq 0 ];then s=$((s+1));fi;echo -ne "[++] Using entry:$c of $l.\r";c=$(($c+1));done;if [ $s -ge 1 ];then echo -e "\n[+] Found $s subdomains.";else echo -e "\n[!!] Found $s subdomains.";fi;else echo -e "[!!] Could not download dictionary from seclists url.";clean;fi
 }
 dictionaryAttackCustom(){
-    check=0;while [ $check -eq 0 ];do read -p "Enter the path of the dictionary file> " dfile;if [ -f "$dfile" ];then istext=$(file $dfile|awk '{print $2}');if [[ $istext = "ASCII" ]];then lenn=$(wc -l $dfile|awk '{print $1}');co=1;su=0;for sub in $(cat $dfile);do host $sub.$1|head -1|grep "has address";if [ $? -eq 0 ];then su=$(($su+1));fi;c=$(($co+1));done;if [ $su -ge 1 ];then echo -e "\n[+] Found $su subdomains.";else echo -e "\n[!!] Found $su subdomains.";fi;check=1;clean;else echo -e "[!!] the file is not ASCII text. Can't use it.";fi;else echo -e "[!!] File $dfile does not exists.";fi;done
+    check=0;while [ $check -eq 0 ];do read -p "Enter the path of the dictionary file> " dfile;if [ -f "$dfile" ];then istext=$(file "$dfile"|awk '{print $2}');if [[ $istext = "ASCII" ]];then lenn=$(wc -l $dfile|awk '{print $1}');co=1;su=0;for sub in $(cat "$dfile");do host "$sub"."$1"|head -1|grep "has address";if [ $? -eq 0 ];then su=$((su+1));fi;c=$((co+1));done;if [ $su -ge 1 ];then echo -e "\n[+] Found $su subdomains.";else echo -e "\n[!!] Found $su subdomains.";fi;check=1;clean;else echo -e "[!!] the file is not ASCII text. Can't use it.";fi;else echo -e "[!!] File $dfile does not exists.";fi;done
 }
 bruteForceDNS(){
     echo -e "[++] Fuzzing subdomains of $1\n[?] Do yo want to use a custom dictionary? [C=custom/d=Default]\n[?] Default: Provides a dictionary with the top 1000 of the most commonly used subdomains.\nCustom: Use your own custom dictionary.";while true; do read -rp "[D/c]> " dc;case $dc in
-            [Dd]* ) dictionaryAttack $1; break;;
-            [Cc]* ) dictionaryAttackCustom $1; break;;
+            [Dd]* ) dictionaryAttack "$1"; break;;
+            [Cc]* ) dictionaryAttackCustom "$1"; break;;
             * ) echo -e "[!!] Please answer D or C\n";;esac;done
 }
 crtSH(){
